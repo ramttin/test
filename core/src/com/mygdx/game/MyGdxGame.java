06d2817ad017b73e1b36ebe4f3bc3ac848e1b44d
ramttin <ramttin@gmail.com>
2018-01-12 19:47:13 +0330

    shooting

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -17,11 +17,15 @@ public class MyGdxGame extends ApplicationAdapter {
     Texture img;
     float W;
     float H;
-    int deg;
+    float deg;
     Circle Aim;
     Circle ball;
     Boolean fired;
-
+    float Vx;
+    float Vy;
+    float Vtot = 20;
+    float Locx;
+    float Locy;
 
 
     ShapeRenderer renderer;
@@ -36,40 +40,57 @@ public class MyGdxGame extends ApplicationAdapter {
         renderer = new ShapeRenderer();
         ball = new Circle();
         Aim = new Circle();
+        fired = false;
     }
-
     @Override
     public void render() {
-//		if (input.justTouched()){
-//			float acc= input.getAzimuth();
-//			Gdx.app.log("hello",String.valueOf(acc));
-//
-//		}
-        batch.begin();
 
-        batch.end();
-        deg=deg+5;
+        if (!fired) {
+            Locx = 50 * W;
+            Locy = 50 * H;
+
+//        batch.end();
+            deg = deg + 5f;
 //        Gdx.app.log("degree",String.valueOf(deg));
-        renderer.begin(ShapeRenderer.ShapeType.Filled);
-        renderer.setColor(0, 20, 20, 20);
-        ball.set((int)(50 * W), (int)(50 * H), (int)(2 * H));
+            renderer.begin(ShapeRenderer.ShapeType.Filled);
+            renderer.setColor(0, 20, 20, 20);
+            ball.set((int) (Locx), (int) (Locy), (int) (2 * H));
 //        ball.set(100, 100, 15);
-        renderer.circle(ball.x, ball.y, ball.radius);
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        renderer.end();
-        renderer.begin(ShapeRenderer.ShapeType.Filled);
-        renderer.setColor(250,0,0,1);
-        Gdx.app.log("degree",String.valueOf((1)*(Math.cos((deg)))));
-        Gdx.app.log("degree2",String.valueOf(deg));
+            renderer.circle(ball.x, ball.y, ball.radius);
+            Gdx.gl.glClearColor(1, 1, 1, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            renderer.end();
+            renderer.begin(ShapeRenderer.ShapeType.Filled);
+            renderer.setColor(250, 0, 0, 1);
+            Gdx.app.log("degree", String.valueOf((1) * (Math.cos((deg)))));
+            Gdx.app.log("degree2", String.valueOf(deg));
 
-        Aim.set((int)((6+ball.radius)*(Math.cos((deg*3.14f/180f)))+ball.x),(int)((6+ball.radius)*(Math.sin((float)(deg*Math.PI/180f))))+ball.y,6);
+            Aim.set((int) ((6 + ball.radius) * (Math.cos((deg * 3.14f / 180f))) + ball.x), (int) ((6 + ball.radius) * (Math.sin((float) (deg * Math.PI / 180f)))) + ball.y, 6);
 //        Aim.set(ball.x,ball.y,4);
 //		batch.draw(img,50*W,50*H,40*R,40*R);
 //        Aim.set(deg,deg,40);
-        renderer.circle(Aim.x, Aim.y, Aim.radius);
+            renderer.circle(Aim.x, Aim.y, Aim.radius);
 
+//        batch.begin();
+        } else {
+            Vx = Vtot * (float) Math.cos(deg * 3.14f / 180f);
+            Vy = Vtot * (float) Math.sin(deg * 3.14f / 180f);
+            Locx+=Vx;
+            Locy+=Vy;
+
+            renderer.begin(ShapeRenderer.ShapeType.Filled);
+            renderer.setColor(0, 20, 20, 20);
+            ball.set((int) (Locx), (int) (Locy), (int) (2 * H));
+            renderer.circle(ball.x, ball.y, ball.radius);
+            Gdx.gl.glClearColor(1, 1, 1, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            renderer.end();
+
+        }
         renderer.end();
+        if (input.justTouched()) {
+            fired = true;
+        }
 
     }
 
