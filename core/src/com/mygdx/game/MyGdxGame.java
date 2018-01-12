d35b093f00b49d67252ca016b3eeffbcb1c7a740
ramttin <ramttin@gmail.com>
2018-01-12 13:17:28 +0330

    hi

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -16,6 +16,10 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		batch = new SpriteBatch();
+
+
+
 		img = new Texture("badlogic.jpg");
 	}
 
