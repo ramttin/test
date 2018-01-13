4c7264349aa5e6447de981ddfb54e7c162d29cae
mates <mahdisajjadi70@gmail.com>
2018-01-14 03:17:17 +0330

    day2

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -22,39 +22,39 @@ import static com.badlogic.gdx.Gdx.input;
 
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
-    Texture img;
-    float W;
+    Texture Heart, DeadHeart, RIGHTWALL, LEFTWALL;
+    float W, G;
     float H;
     float deg;
     Circle Aim;
     Circle ball;
-    Boolean fired,JUSTHIT;
-    Boolean justloaded,gameover;
-    float Vx,EnemyVx;
-    float Vy,EnemyVy;
+    Boolean fired, JUSTHIT;
+    Boolean justloaded, gameover;
+    float Vx, EnemyVx;
+    float Vy, EnemyVy;
     float Vtot = 15;
-    float Locx,waiting;
-    float Locy,changing;
+    float Locx, waiting;
+    float Locy, changing;
     Rectangle[] walls;
     Circle Enemy;
     int[][] wallsM = {{0, 0}, {0, 1900}, {900, 900}, {0, 0}};
-    int bazel;
+    int bazel, distancesX, distancesY;
     Circle dot;
     int dotX;
     int dotY;
-    int SCORE,HEARTS;
+    int SCORE, HEARTS;
     ShapeRenderer renderer;
-    BitmapFont font,fontbig;
-    int EnemyX,EnemyY;
+    BitmapFont font, fontbig;
+    int EnemyX, EnemyY;
 
 
     @Override
     public void create() {
 //        Sound oggSound = Gdx.audio.newSound(Gdx.files.internal("data/ogg.ogg"));
 //        TextArea
-        EnemyVx=4;
-        EnemyVy=0;
-        bazel = 50;
+        EnemyVx = 4;
+        EnemyVy = 0;
+        bazel = 120;
         walls = new Rectangle[4];
         font = new BitmapFont();
         font.getData().setScale(2);
@@ -64,22 +64,25 @@ public class MyGdxGame extends ApplicationAdapter {
         fontbig.setColor(Color.WHITE);
 
         batch = new SpriteBatch();
-//        img = new Texture("circle.png");
+        Heart = new Texture("hearty.png");
+        DeadHeart = new Texture("bad_heart.png");
+        RIGHTWALL = new Texture("wrighty.png");
+        LEFTWALL = new Texture("wlefty.png");
         W = Gdx.graphics.getWidth() / 100f;
         H = Gdx.graphics.getHeight() / 100f;
-        dotX=(int)(Math.random()*(100*W-4f*bazel)+2f*bazel);
-        dotY=(int)(Math.random()*(100*H-4f*bazel)+2f*bazel);
-        EnemyX=(int)(Math.random()*2f);
-        EnemyY=(int)(Math.random()*2f);
-        if(EnemyX>1){
-            EnemyX=(int)((100*H-2*bazel)-35*H*Math.random());
-        }else {
-            EnemyX=(int)((2*bazel)+35*H*Math.random());
+        dotX = (int) (Math.random() * (100 * W - 4f * bazel) + 2f * bazel);
+        dotY = (int) (Math.random() * (100 * H - 4f * bazel) + 2f * bazel);
+        EnemyX = (int) (Math.random() * 2f);
+        EnemyY = (int) (Math.random() * 2f);
+        if (EnemyX > 1) {
+            EnemyX = (int) ((100 * H - 2 * bazel) - 35 * H * Math.random());
+        } else {
+            EnemyX = (int) ((2 * bazel) + 35 * H * Math.random());
         }
-        if(EnemyY>1){
-            EnemyY=(int)((100*W-2*bazel)-35*W*Math.random());
-        }else {
-            EnemyY=(int)((2*bazel)+35*W*Math.random());
+        if (EnemyY > 1) {
+            EnemyY = (int) ((100 * W - 2 * bazel) - 35 * W * Math.random());
+        } else {
+            EnemyY = (int) ((2 * bazel) + 35 * W * Math.random());
         }
         int[] wallsH = {(int) (100 * H), bazel, (int) (100 * H), bazel};
         int[] wallsW = {bazel, (int) (100 * W), bazel, (int) (100 * W)};
@@ -88,8 +91,8 @@ public class MyGdxGame extends ApplicationAdapter {
         ball = new Circle();
         dot = new Circle();
         Enemy = new Circle();
-        SCORE=0;
-        HEARTS=3;
+        SCORE = 0;
+        HEARTS = 3;
         Aim = new Circle();
         for (int i = 0; i < 4; i++) {
             walls[i] = new Rectangle(wallsM[i][0], wallsM[i][1], wallsW[i], wallsH[i]);
@@ -100,35 +103,62 @@ public class MyGdxGame extends ApplicationAdapter {
 //        wallsH=
         justloaded = true;
         fired = false;
-        JUSTHIT=false;
-        gameover=false;
-        changing=0;
+        JUSTHIT = false;
+        gameover = false;
+        changing = 0;
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0 + SCORE / 100f, 0 + SCORE / 100f, 0.2f + SCORE / 100f, 1);
+
+        Gdx.gl.glClearColor(0.254901961f + SCORE / 100f, 0.57254902f+ SCORE / 100f, 0.84f + SCORE / 100f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+//        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+//        Gdx.graphics.getGL20().glEnable(GL20.GL_BLEND); // Or GL20
         if (gameover) {
+            distancesX=(int)(50*W);
+            distancesY=(int)(50*H);
             changing++;
+            Gdx.gl.glEnable(GL20.GL_BLEND);
+            Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+            JUSTHIT = false;
+
             renderer.begin(ShapeRenderer.ShapeType.Filled);
-            renderer.setColor(0.1f, 0.9f-changing*0.01f, 0.1f+changing*0.01f, changing*0.001f);
-            renderer.rect(0,0,100*W,100*H);
+            renderer.setColor(0.1f, 0.9f - changing * 0.01f, 0.1f + changing * 0.01f, changing * 0.001f);
+            renderer.rect(0, 0, 100 * W, 100 * H);
+
             renderer.end();
+            Gdx.gl.glDisable(GL20.GL_BLEND);
             batch.begin();
             fontbig.draw(batch, "G A M E   O V E R", 50 * W - 240, 50 * H);
-            fontbig.draw(batch, "SCORE  :  "+String.valueOf(SCORE), 50 * W - 185, 50 * H-90);
+            fontbig.draw(batch, "SCORE  :  " + String.valueOf(SCORE), 50 * W - 185, 50 * H - 90);
             batch.end();
             renderer.end();
             if (input.justTouched()) {
-                gameover=false;
-                fired=false;
-                HEARTS=3;
-                SCORE=0;
+
+
+                gameover = false;
+                fired = false;
+                HEARTS = 3;
+                SCORE = 0;
+
+
+
             }
         } else {
-            if(HEARTS<1){
-                gameover=true;
+            if (HEARTS < 1) {
+                gameover = true;
+            }
+            for (int i = 0; i < 3; i++) {
+                batch.begin();
+                if (i + 1 <= HEARTS) {
+                    batch.draw(Heart, (int) 100 * W - (int) (bazel * 0.8f * (i + 2)), (int) 100 * H - 1.7f* bazel , 60, 60);
+                } else {
+                    batch.draw(DeadHeart, (int) 100 * W - (int) (bazel * 0.8f * (i + 2)), (int) 100 * H - 1.7f* bazel, 60, 60);
+                }
+                batch.end();
             }
 
 
@@ -141,35 +171,46 @@ public class MyGdxGame extends ApplicationAdapter {
             }
             batch.begin();
 //        font.setColor(Color.WHITE);
-            font.draw(batch, "SCORE :" + String.valueOf(SCORE), 2 * bazel, 100 * H - 2 * bazel);
-            font.draw(batch, "LIFE :" + String.valueOf(HEARTS), 100 * W - 4 * bazel, 100 * H - 2 * bazel);
+            font.draw(batch, "SCORE :" + String.valueOf(SCORE),  bazel+30, 100 * H - 1.2f * bazel);
+//            font.draw(batch, "LIFE :" + String.valueOf(HEARTS), 100 * W - 1.2f* bazel, 100 * H - 1.2f * bazel);
 
             batch.end();
             renderer.begin(ShapeRenderer.ShapeType.Filled);
-            renderer.setColor(0.1f, 0.6f, 0.4f, 1);
+            renderer.setColor(0.1f, 0.68f, 0.31f, 1);
+
             for (int i = 0; i < 4; i++) {
                 renderer.rect(walls[i].x, walls[i].y, walls[i].getWidth(), walls[i].getHeight());
+
             }
             renderer.end();
+            batch.begin();
+            int wallsheight=bazel * LEFTWALL.getHeight() / LEFTWALL.getWidth();
+            for(int i=0;i<(int)(1+100*H/wallsheight);i++){
+                batch.draw(LEFTWALL, walls[0].x, walls[0].y+bazel + i*wallsheight, (float) bazel, bazel * LEFTWALL.getHeight() / LEFTWALL.getWidth());
+            }
+            for(int i=0;i<(int)(1+100*H/wallsheight);i++){
+                batch.draw(RIGHTWALL, walls[2].x, walls[0].y +bazel+ i*wallsheight, (float) bazel, bazel * LEFTWALL.getHeight() / LEFTWALL.getWidth());
+            }
+            batch.end();
 
             if (!fired) {
                 Locx = 50 * W;
                 Locy = 50 * H;
-                changing=0;
+                changing = 0;
 
-                deg = deg +1f;
+                deg = deg + 1f;
                 renderer.begin(ShapeRenderer.ShapeType.Filled);
-                renderer.setColor(0, 20, 20, 20);
+                renderer.setColor(0.2f, 0.8f, 0.7f, 0.5f);
                 ball.set((int) (Locx), (int) (Locy), (int) (2 * H));
                 renderer.circle(ball.x, ball.y, ball.radius);
                 renderer.end();
-                renderer.begin(ShapeRenderer.ShapeType.Filled);
-                renderer.setColor(250, 0, 0, 1);
-                Aim.set((int) ((6 + ball.radius) * (Math.cos((deg * 3.14f / 180f))) + ball.x), (int) ((6 + ball.radius) * (Math.sin((float) (deg * Math.PI / 180f)))) + ball.y, 6);
-                renderer.circle(Aim.x, Aim.y, Aim.radius);
-                renderer.end();
-                Vx =- Vtot * (float) Math.cos(deg * 3.14f / 180f);
-                Vy = Vtot * (float) Math.sin(deg * 3.14f / 180f);
+//                renderer.begin(ShapeRenderer.ShapeType.Filled);
+//                renderer.setColor(250, 0, 0, 1);
+//                Aim.set((int) ((6 + ball.radius) * (Math.cos((deg * 3.14f / 180f))) + ball.x), (int) ((6 + ball.radius) * (Math.sin((float) (deg * Math.PI / 180f)))) + ball.y, 6);
+//                renderer.circle(Aim.x, Aim.y, Aim.radius);
+//                renderer.end();
+//                Vx =- Vtot * (float) Math.cos(deg * 3.14f / 180f);
+//                Vy = Vtot * (float) Math.sin(deg * 3.14f / 180f);
 
 
                 renderer.begin(ShapeRenderer.ShapeType.Filled);
@@ -177,7 +218,28 @@ public class MyGdxGame extends ApplicationAdapter {
                 Enemy.set(EnemyX, EnemyY, (int) (2 * H));
                 renderer.circle(Enemy.x, Enemy.y, Enemy.radius);
                 renderer.end();
+                if (Gdx.input.isTouched()) {
+                    for (int i = 0; i < 10; i++) {
+                        distancesX = (int) (Gdx.input.getX() - ball.x);
+                        distancesY = (int) (Gdx.input.getY() - ball.y);
+                        renderer.begin(ShapeRenderer.ShapeType.Filled);
+                        renderer.setColor(1, 1, 1, 1);
+                        renderer.circle(ball.x + distancesX * (i + 1) / 10, ball.y - distancesY * (i + 1) / 10, (i + 10));
+                        renderer.end();
 
+                    }
+                }
+                if ((distancesX * distancesX + distancesY * distancesY) > 50) {
+//            readytofire=True;
+//        }
+//            if(readytofire) {
+                    if (!Gdx.input.isTouched()) {
+                        fired = true;
+                        Vx = Vtot * (float) distancesX / (float) Math.sqrt((distancesX * distancesX + distancesY * distancesY));
+                        Vy = -Vtot * (float) distancesY / (float) Math.sqrt((distancesX * distancesX + distancesY * distancesY));
+
+                    }
+                }
             } else {
 
                 dot.set(dotX, dotY, 4);
@@ -211,16 +273,30 @@ public class MyGdxGame extends ApplicationAdapter {
                 Locy += Vy;
 
                 renderer.begin(ShapeRenderer.ShapeType.Filled);
-                renderer.setColor(0.8f, (float) (0.8 * (120 - waiting) / 120f), (float) (0.8 * (120 - waiting) / 120f), 0);
+                if (JUSTHIT & waiting % 20 > 9) {
+                    renderer.setColor(0.1f, 0.1f, 1, 0.0f);
+                    batch.begin();
+                    batch.getColor().a = 0.5f;
+//                    batch.draw(Heart, (int) 100 * W - (int) (bazel * 1.5 * (HEARTS + 2)), (int) 100 * H - 2 * bazel * 2, 60, 60);
+                    batch.draw(Heart, (int) 100 * W - (int) (bazel * 0.8f * (HEARTS + 2)), (int) 100 * H - 1.7f* bazel, 60, 60);
+
+                    batch.end();
+                } else {
+                    renderer.setColor(0.8f, 0.8f, 0.8f, 1);
+
+                }
                 ball.set((int) (Locx), (int) (Locy), (int) (2 * H + SCORE * 2));
                 renderer.circle(ball.x, ball.y, ball.radius);
                 renderer.end();
 
             }
+
             renderer.end();
-            if (input.justTouched()) {
-                fired = true;
-            }
+//            if (input.justTouched()) {
+//                fired = true;
+//            }
+
+
             if (fired) {
                 if (input.justTouched()) {
                     Vx = -Vx;
@@ -293,12 +369,13 @@ public class MyGdxGame extends ApplicationAdapter {
 
 
         }
+        Gdx.gl.glDisable(GL20.GL_BLEND);
     }
 
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+//        img.dispose();
     }
 }
